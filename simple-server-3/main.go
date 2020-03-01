6bf7f270893274f153f93eac6826497bebfa72f0
Thom Maurick <43498912+MaurickThom@users.noreply.github.com>
2020-02-29 19:51:22 -0500

    Update main.go

--- simple-server-3/main.go
+++ simple-server-3/main.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	var mux = http.NewServeMux()                 // mutiplexor o roteador
+	var mux = http.NewServeMux()                 // multiplexor o roteador
 	var fs = http.FileServer(http.Dir("public")) //public
 	mux.Handle("/", fs)
 	http.ListenAndServe(":8080", mux)
